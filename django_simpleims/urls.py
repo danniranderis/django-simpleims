5efce87f54b642a98a763b69041108e19c3a976a
Danni Randeris <danni@danniranderis.dk>
2021-09-14 20:37:16 +0200

    Added SB Admin 2 as design-template and setup for template-inheritance.

--- django_simpleims/urls.py
+++ django_simpleims/urls.py
@@ -20,6 +20,7 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('admin/', admin.site.urls),
 ]
 
